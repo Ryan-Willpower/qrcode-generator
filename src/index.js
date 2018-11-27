d3fee58b71f10f1c1625fcc1a9dda5ca369035f8
Ryan-Willpower <surasak.c.work@gmail.com>
2018-11-27 17:35:22 +0700

    [add] hooks

--- src/index.js
+++ src/index.js
@@ -1,35 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import QRCode from 'qrcode'
 import * as serviceWorker from './serviceWorker';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      qrUri: ''
-    }
-  }
+const App = () => {
+  const [ uri, setUri ] = useState('')
 
-  componentDidMount = async () => {
-    this.setState({
-      qrUri: await this.generateQR('https://www.google.co.th')
-    })
-    console.log(this.state.qrUri)
-  }
+  useEffect(() => {
+    (async () => {
+      setUri(await QRCode.toDataURL('https://www.google.co.th'))
+    })()
+  })
 
-  generateQR = async text => {
-    try {
-      let qrUri = await QRCode.toDataURL(text)
-      return qrUri
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  render() {
-    return <img src={this.state.qrUri} alt="eiei" />
-  }
+  return <img src={uri} alt="google" />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
